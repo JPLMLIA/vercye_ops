97d0197f811d0bab38926b960983a21f3011ed89
rohansaw <rohan.sawahn@posteo.de>
2025-02-11 14:59:59 +0100

    adapted naming of regions to deal with more special characters. Added handling of different croptypes in sim matching form apsim in the pipeline

--- vercye_ops/lai/0_build_library.py
+++ vercye_ops/lai/0_build_library.py
@@ -4,6 +4,7 @@ import click
 import geopandas as gpd
 
 import logging
+import re
 
 logging.basicConfig(level=logging.INFO)
 
@@ -38,7 +39,8 @@ def convert_shapefile_to_geojson(shp_fpath, admin_level, output_head_dir, verbos
     if gdf.empty:
         raise ValueError("The shapefile does not contain any polygons.")
     if gdf.crs.to_epsg() != 4326:
-        raise ValueError("The shapefile coordinate system is not WGS 84.")
+        gdf = gdf.to_crs(epsg=4326)
+        #raise ValueError("The shapefile coordinate system is not WGS 84.")
     
     if verbose:
         logging.info('Processing %i %s regions.', len(gdf), admin_level)
@@ -52,8 +54,9 @@ def convert_shapefile_to_geojson(shp_fpath, admin_level, output_head_dir, verbos
         else:
             region_name = row["NAME_2"]
 
-        # Take out any apostrophes as these cause headaches down the line with scripting the filename processing
-        region_name = region_name.replace("'", "")
+        # Take out any apostrophes and other special chars as these cause headaches down the line with scripting the filename processing
+        region_name = region_name.replace("'", "").replace('"', "")
+        region_name = re.sub(r"[^\w.-]", "_", region_name)
 
         output_fpath = output_head_dir / Path(f'{region_name}.geojson')
 
