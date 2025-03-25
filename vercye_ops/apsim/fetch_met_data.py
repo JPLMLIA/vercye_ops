b25dd31811db9c4ab7706a062ab91fe1f4458b8e
Rohan Sawahn <49531442+rohansaw@users.noreply.github.com>
2025-03-25 08:05:46 +0100

    remove debug statement backoff for request

--- vercye_ops/apsim/fetch_met_data.py
+++ vercye_ops/apsim/fetch_met_data.py
@@ -106,7 +106,6 @@ def fetch_nasa_power_data(start_date, end_date, variables, lon, lat):
     variables_str = ','.join(variables)
 
     # API endpoint and parameters
-    time.sleep(300)
     url = "https://power.larc.nasa.gov/api/temporal/daily/point"
     params = {'parameters': variables_str,
               'community': 'AG',
