50709a86b495a976c668ea6ba2479a6a2cd0edb8
rohansaw <rohan.sawahn@posteo.de>
2025-03-27 10:17:14 +0100

    removing double info from matching report

--- vercye_ops/matching_sim_real/generate_matching_report.py
+++ vercye_ops/matching_sim_real/generate_matching_report.py
@@ -131,7 +131,6 @@ def generate_report(apsim_filtered_fpath, rs_lai_csv_fpath, apsim_db_fpath, tota
     start_date = mean_data.index.min().strftime('%Y-%m-%d')
     end_date = mean_data.index.max().strftime('%Y-%m-%d')
     n_simulations = len(good_sim_ids)
-    n_days_with_rs_data = rs_df[rs_df['interpolated'] == 0].shape[0]
     n_days_with_rs_data_valid =  rs_df[(rs_df['interpolated'] == 0) & (rs_df['Cloud or Snow Percentage'] < 100)].shape[0]
     cloud_snow_percentage = rs_df[(rs_df['interpolated'] == 0) & (rs_df['Cloud or Snow Percentage'] < 100)]['Cloud or Snow Percentage'].mean()
     
@@ -142,7 +141,6 @@ def generate_report(apsim_filtered_fpath, rs_lai_csv_fpath, apsim_db_fpath, tota
                   f"Date range: {start_date} to {end_date}<br>"
                   f"Mean yield rate: {mean_yield_kg_ha:0.0f} kg/ha<br>"
                   f"Production: <b>{total_yield_metric_tons:0.0f} metric tons</b><br>"
-                  f"Days with RS data (including 100% cloud coverage): {n_days_with_rs_data} days<br>"
                   f"Valid Days with RS data (< 100% cloud coverage): {n_days_with_rs_data_valid} days<br>"
                   f"Average Cloud/Snow coverage per non-interpolated valid RS date: {cloud_snow_percentage:0.2f}%")
 
